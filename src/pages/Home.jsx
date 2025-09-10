0b13644e76bcfb4fb21e1ae997fa1f1c5fc3a5cf
hashemUOP <167630882+hashemUOP@users.noreply.github.com>
2025-09-10 13:29:23 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+function Home() {
+  return (
+    
+    <Spline scene="https://prod.spline.design/hSQJasOpaY3o7AVA/scene.splinecode" />
+
+  )
+}
+
+export default Home
